8156400f3331a2642a562d4475759523f29e5237
sotirelisc <sotirelisc@gmail.com>
2018-06-11 23:42:36 +0300

    Build optimal model with Backward Elimination.

--- Multiple Linear Regression/mlr.py
+++ Multiple Linear Regression/mlr.py
@@ -37,3 +37,65 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Prediction
+y_prediction = regressor.predict(X_test)
+
+# Build the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+# Add b0 constant (required by statsmodels lib, others handle it)
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+
+# Set Significance Level
+SL = 0.05
+X_optimal = X[:, [0, 1, 2, 3, 4, 5]]
+X_modeled = backwardElimination(X_optimal, SL)
+
+# BACKWARD ELIMINATION ALGORITHM STEP BY STEP
+
+X_optimal = X[:, [0, 1, 2, 3, 4, 5]]
+# Create new regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+# Find P-values and choose var with the max one
+regressor_OLS.summary()
+
+# Eliminate it
+X_optimal = X[:, [0, 1, 3, 4, 5]]
+# Create new regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+# Find P-values and choose var with the max one
+regressor_OLS.summary()
+
+# Eliminate it
+X_optimal = X[:, [0, 3, 4, 5]]
+# Create new regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+# Find P-values and choose var with the max one
+regressor_OLS.summary()
+
+# Eliminate it
+X_optimal = X[:, [0, 3, 5]]
+# Create new regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+# Find P-values and choose var with the max one
+regressor_OLS.summary()
+
+# Eliminate it
+X_optimal = X[:, [0, 3]]
+# Create new regressor
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+# Find P-values and choose var with the max one
+regressor_OLS.summary()
